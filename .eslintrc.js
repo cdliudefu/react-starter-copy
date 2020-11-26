d1dfb81d6e5b0ef5b4b37e24710925b99be01bc2
andy.liu <deliu@kenexa.com.cn>
2020-11-26 10:43:53 +0800

    工具备注

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
+// JavaScript 语法规则和代码风格的检查工具
 // ESLint configuration
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
